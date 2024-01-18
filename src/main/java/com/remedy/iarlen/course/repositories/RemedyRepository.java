5572de03f79e161f56e860d3a2ca5d4260a0ae21
Iarlen <iarlensr@hotmail.com>
2024-01-18 01:29:58 -0300

    feat: created custom method findAllByActiveTrue on RemedyRepository.

--- src/main/java/com/remedy/iarlen/course/repositories/RemedyRepository.java
+++ src/main/java/com/remedy/iarlen/course/repositories/RemedyRepository.java
@@ -1,8 +1,13 @@
 package com.remedy.iarlen.course.repositories;
 
 import com.remedy.iarlen.course.models.RemedyModel;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RemedyRepository extends JpaRepository<RemedyModel, Long> {
+
+  List<RemedyModel> findAllByActiveTrue();
 
 }
