a38f0f709951881be239eae9abc52d6574f87e7b
Iarlen <iarlensr@hotmail.com>
2024-01-19 12:23:24 -0300

    feat: created SecurityConfiguration, SecurityFilter and AuthService.

---
+++ src/main/java/com/remedy/iarlen/course/infra/SecurityFilter.java
@@ -0,0 +1,57 @@
+package com.remedy.iarlen.course.infra;
+
+import com.remedy.iarlen.course.repositories.UserRepository;
+import com.remedy.iarlen.course.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var token = recoverToken(request);
+
+        if(token != null) {
+            var subject = this.tokenService.verifyToken(token);
+
+            var user = this.userRepository.findByUsername(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        };
+
+        return null;
+    };
+}
